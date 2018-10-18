bf99dfdbf12d8cae383612d24e9405ebef846555
howardop <hlo4@cornell.edu>
2018-10-18 11:48:57 -0400

    Google Maps integrated with App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
-import Map from './components/Map.js'
 
 const defaultCenter = {
   // Broadway and W46th St
@@ -19,21 +17,36 @@ class App extends Component {
     zoom: defaultZoom    
   }
 
+
   componentDidMount() {
+    this.renderMap();
     this.getLocations();
   }
-/*
+
+  renderMap = () => {
+    addGoogleApiScriptTag('https://maps.googleapis.com/maps/api/js?key=AIzaSyAubhdURq6tefzCuxspVifGrzXFveOM4Fg&callback=initMap');
+    window.initMap = this.initMap;
+  }
+
   initMap = () => {
     let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: defaultCenter,
+      center:  defaultCenter,
       zoom: defaultZoom
     });
   }
-*/
+
+  addMarkers = () => {
+    console.log(`Locations have been loaded into local state`);
+    
+    this.state.locations.map(theater => {
+      console.log(`This theater is ${theater.name}`);
+    })
+    
+  }
   getLocations = () => {
     // Look for theaters on Broadway
     const url = "https://api.foursquare.com/v2/venues/search?ll=40.7579984,%20-73.9856257&radius=150&intent=browse&client_id=DQ4DTQOWUBJMWTFUFL4YXGDHMV0K4TE13IHYDFJPER15UTJB&client_secret=A3ZVRPXB2ENUSG5EG5BZM0UYVR1JW33TUXQKX2TGSJRB2B4W&v=20181001&query=theater";
-    let theaters = [];
+    
     console.log(`On entry to getLocations, "this" is `, this);
     fetch(url)
       .then( (response) => {
@@ -48,10 +61,10 @@ class App extends Component {
         // Foursquare returns more than just theaters
         let theaters = this.state.locations.filter(function (value, index, array) {
           console.log(`Name is ${value.name}.  Type=${value.categories[0].name}.` );
-          if (value.categories[0].name == 'Theater') {
-            console.log("value.categories[0].name == 'Theater' is ", (value.categories[0].name == 'Theater'));
+          if (value.categories[0].name === 'Theater') {
+            console.log("value.categories[0].name === 'Theater' is ", (value.categories[0].name === 'Theater'));
           }
-          return value.categories[0].name == 'Theater';
+          return value.categories[0].name === 'Theater';
           //return value.categories[0].name.endsWith('Theater');
         });
         console.log(`There are ${theaters.length} real theaters`);
@@ -66,56 +79,31 @@ class App extends Component {
         }
         
       });
- 
+
   }
 
-  addMarkers = () => {
-    console.log(`Locations have been loaded into local state`);
-    
-    this.state.locations.map(theater => {
-      console.log(`This theater is ${theater.name}`);
-    })
-    
-  }
-
-  handleMarkerClickEvent = (event, latlng, index) => {
-    this.setState({
-      selectMarkerIndex: index,
-      center: latlng // the clicked or selected marker latlng
-    });
-  }
-
-  handleInfoWindowCloseEvent = (event) => {
-    // Reset the state values to the default
-    this.setState({
-      selectedMarkerIndex: -1,
-      center: defaultCenter,  // the default latlng for the application
-      zoom: defaultZoom
-    })
-  }
-/*
-  // we want both of the components to refer to the same funciton inside App.js
-  <ListPlaces onKeyPressed=this.handleMarkerClickEvent 
-    locationArray={// Locations or venues that we get from Foursquare API} />
-  <Map onMarkerClick=this.handleMarkerClickEvent />
-
-  <Map role='application'>
-    // the marker click will be done from inside CompositeMAPUsingReact
-    <CompositeMAPUsingReact onMarkerClick=this.props.onMarkerClick />
-  </Map>
-*/
-
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Neighborhood Map</h1>
-        </header>
-        <Map locations={this.state.locations} />
-       </div>
+      <main>
+        <div className = "App" >
+          <header className = "App-header" >
+            <h1 className = "App-title" > Neighborhood Map </h1> 
+          </header> 
+        <div id = 'map' > </div>  
+        </div> 
+      </main>
     );
-  }
+  };
 }
+
+function addGoogleApiScriptTag(url) {
+  let index = window.document.getElementsByTagName('script')[0];
+  let script = window.document.createElement('script');
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
+};
 
 export default App;
