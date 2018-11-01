30236701b38ee39fde786a92651265d096ed5f97
howardop <hlo4@cornell.edu>
2018-11-01 19:01:33 -0400

    Can populate SideBar and identify list items on map

--- src/App.js
+++ src/App.js
@@ -8,14 +8,30 @@ const defaultCenter = {
   lat: 40.7579984,
   lng: -73.9856257
 };
-
 const defaultZoom = 17;
 
+// Hide points of interest that otherwise clutter up the map
+var myStyles =[
+  {
+      featureType: "poi",
+      elementType: "labels",
+      stylers: [
+            { visibility: "off" }
+      ]
+  }
+];
 
+var myOptions = {
+  zoom: defaultZoom,
+  center: defaultCenter,
+  mapTypeId: window.google.maps.MapTypeId.ROADMAP,
+  styles: myStyles 
+};
 
 class App extends Component {
   state = {
     locations: [],
+    filteredLocations: [],
     selectedMarkerIndex: -1,
     center: defaultCenter,
     zoom: defaultZoom,
@@ -28,10 +44,11 @@ class App extends Component {
   }
 
   initMap = () => {
-    let map = new window.google.maps.Map(document.getElementById('map'), {
+    /*let map = new window.google.maps.Map(document.getElementById('map'), {
       center:  defaultCenter,
       zoom: defaultZoom
-    });
+    });*/
+    let map = new window.google.maps.Map(document.getElementById('map'), myOptions);
 
     // Now set up InfoWindow to be shared by each marker
     let infoWindow = new window.google.maps.InfoWindow();
@@ -39,7 +56,7 @@ class App extends Component {
     // this is pointing to window here
     // Add marker for each theater found
     let markers = this.state.locations.map(theater => {
-      console.log(theater.location.formattedAddress);
+      //console.log(theater.location.formattedAddress);
       let contentString = `<strong>${theater.name}</strong><br /> ${theater.location.address}`;
       let marker = new window.google.maps.Marker({
         position: {
@@ -53,65 +70,69 @@ class App extends Component {
 
       // Set up Listener for each marker to display InfoWindow when user clicks on marker
       marker.addListener('click', function () {
-      // Set content for specific marker
-      infoWindow.setContent(contentString);
-      infoWindow.open(map, marker);
+        // Set content for specific marker
+        console.log('marker.addListener called');
+        infoWindow.setContent(contentString);
+        infoWindow.open(map, marker);
       });
 
       // Include return statement to avoid eslint message
-      return markers;
+      return marker;
+
     });
 
+    console.log('About to put markers into state');
     this.setState({
       map: map,
       markers: markers,
       google: window.google
     });
+    
+
   }
 
   getLocations = () => {
     // Look for theaters on Broadway
-    // Use explore instead of search?
-    const url = "https://api.foursquare.com/v2/venues/search?ll=40.7579984,%20-73.9856257&radius=150&intent=browse&client_id=DQ4DTQOWUBJMWTFUFL4YXGDHMV0K4TE13IHYDFJPER15UTJB&client_secret=A3ZVRPXB2ENUSG5EG5BZM0UYVR1JW33TUXQKX2TGSJRB2B4W&v=20181001&query=theater";
+    const theaterCategoryId = '4bf58dd8d48988d137941735'; 
+    
+    const url = `https://api.foursquare.com/v2/venues/search?ll=40.7579984,%20-73.9856257&radius=250&intent=browse&client_id=DQ4DTQOWUBJMWTFUFL4YXGDHMV0K4TE13IHYDFJPER15UTJB&client_secret=A3ZVRPXB2ENUSG5EG5BZM0UYVR1JW33TUXQKX2TGSJRB2B4W&v=20181001&categoryId=${theaterCategoryId}`;
     
     // this points to App here
-    console.log(`On entry to getLocations, "this" is `, this);
+    //console.log(`On entry to getLocations, "this" is `, this);
     fetch(url)
       .then( (response) => {
-        console.log(`In fetch.then(response), "this" is `, this);
-        console.log('Data returned');
+        //console.log(`In fetch.then(response), "this" is `, this);
+        //console.log('Data returned');
         return response.json();
       })
       .then ((data) => {
         // this points to App here
-        console.log(`In fetch.then(data), "this" is `, this);
+        //console.log(`In fetch.then(data), "this" is `, this);
         this.setState({locations: data.response.venues});
-        console.log(`There are ${this.state.locations.length} items returned`);
+        //console.log(`There are ${this.state.locations.length} items returned`);
         // Foursquare returns more than just theaters
         let theaters = this.state.locations.filter(function (value, index, array) {
-          console.log(`Name is ${value.name}.  Type=${value.categories[0].name}.` );
-          if (value.categories[0].name === 'Theater') {
-            console.log("value.categories[0].name === 'Theater' is ", (value.categories[0].name === 'Theater'));
-          }
-          return value.categories[0].name === 'Theater';
-          //return value.categories[0].name.endsWith('Theater');
+          return (value.name.includes('Theater') || value.name.includes('Theatre'));
         });
+        /*
         console.log(`There are ${theaters.length} real theaters`);
         console.log(`the theaters are `);
         for (let i=0; i<theaters.length; i++) {
           console.log(theaters[i].name);
         }
+        */
 
         // Update state with real theaters and create map
         this.setState({locations: theaters}, function() {
           // Cannot not just pass initMap(), must pass a function that calls initMap()
           this.initMap();
         });
-        
+        /*
         console.log(`the theaters in state.locations are `);
         for (let i=0; i<theaters.length; i++) {
           console.log(`${i+1}. `, this.state.locations[i].name);
         }  
+        */
       });
  
       /*
@@ -134,12 +155,10 @@ class App extends Component {
   }
 
   toggleSideBar = () => {
-    console.log('toggleSideBar called');
     this.setState({open: !this.state.open});
   }
 
   closeSideBar = () => {
-    console.log('closeSideBar called');
     this.setState({open: false});
   }
   
@@ -150,35 +169,47 @@ class App extends Component {
       position: "absolute",
       left: 10,
       top: 20,
-      background: "white",
+      background: "black",
+      color: 'white',
       padding: 10
     },
-    hide: {
-      display: 'none'
-    },
-    header: {
-      marginTop: "0px"
+    hamburger: {
+      fontSize: '2em'
     }
   };
+
+  clickListItem = (index) => {
+    // Set the state to reflect the selected location array index
+    //this.setState({ selectedIndex: index, open: !this.state.open })
+    console.log(`${this.state.locations[index].name} clicked`);
+    console.log(`Need to click marker ${this.state.markers[index].title}`);
+    window.google.maps.event.trigger(this.state.markers[index], 'click');
+    // Need to close SideBar
+    this.closeSideBar();
+    let mev = {
+      stop: null,
+      latlng: defaultCenter
+    }
+    window.google.maps.event.trigger(this.state.map, 'click', mev);
+  }
   
   render() {
     return (
       <main>
         
-        <div className = "App-header">
+        <div className="App-header">
           <button onClick={this.toggleSideBar} style={this.styles.menuButton}>
-            Display Menu
-            <i className="fa fa-bars"></i>
+            <i className="fa fa-bars" style={this.styles.hamburger} ></i>
           </button>
           <h1 className='App-title'><center>Broadway Theater Map</center></h1>
         </div>
            
-          <SideBar locations={this.state.locations}
+          <SideBar id='sidebar' locations={this.state.locations}
           open={this.state.open} 
           toggleSideBar={this.toggleSideBar}
-          close={this.closeSideBar}/>
+          clickListItem={this.clickListItem}/>
 
-          <div id = 'map' > </div>  
+          <div id = 'map' role='application' aria-label='map'> </div>  
        
       </main>
     );
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,32 +1,84 @@
 import React, { Component } from 'react';
 import Drawer from '@material-ui/core/Drawer';
 
-// To eliminate warning about material-ui using the deprecated typography
+// To eliminate material-ui warning about using the deprecated typography
 window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
 
 class SideBar extends Component {
     // "ListDrawer discussion begins at 25:00"
-        //return '<div><strong>Hello</strong></div>';
+      
     state = {
         open: false,
-        query: ''
+        query: '',
+        filteredLocations: []
     }
 
+    // Called each time a character is added to or deleted from the seach field.
+    updateQuery = (query) => {
+        console.log(`query is: ${query}`);
+        
+        let filteredLocations = this.props.locations.filter((loc, index, array) => {return loc.name.toLowerCase().includes(this.state.query.toLowerCase());})
+        console.log('filteredLocations = ', filteredLocations);
+        this.setState({ query, filteredLocations});
+        
+    }
+
+    styles = {
+        list: {
+            width: "250px",
+            padding: "0px 15px 0px"
+        },
+        noBullets: {
+            listStyleType: "none"
+            ,padding: 0
+        },
+        fullList: {
+            width: 'auto'
+        },
+        listItem: {
+            marginBottom: "15px"
+        },
+        listLink: {
+            background: "transparent",
+            border: "none",
+            color: "black"
+        },
+        filterEntry: {
+            border: "1px solid gray",
+            padding: "3px",
+            margin: "30px 0px 10px",
+            width: "100%"
+        }
+    };
+
     render() {
-        console.log('Locations passed to SideBar', this.props.locations);
         return(
             <div>
                 <Drawer  open={this.props.open} onClose={this.props.toggleSideBar}>  
-                <ol>
-                    <li> tom </li>
-                    <li> dick </li>
-                    <li> harry </li>
-                </ol>   
-                </Drawer>   
-                    
+                    <div style={this.styles.list}>
+                        <input
+                            type = "text"
+                            placeholder = "Search for Theater"
+                            value = {this.state.query}
+                            onChange = {(event) => {
+                                this.updateQuery(event.target.value);
+                            }}
+                            style = {this.styles.filterEntry}
+                        />                
+                        <ul >
+                            {this.props.locations &&
+                            this.props.locations.map((location, index) => {
+                                return (
+                                    <li key={index}>
+                                        <button style={this.styles.listLink} key={index} onClick={e => this.props.clickListItem(index)}>{location.name}</button>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </Drawer>           
             </div>
-        )
-        
+        )      
     }
 };
 
